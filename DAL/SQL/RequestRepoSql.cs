cc3198dc9ec54b0ff2d167971707c9eda0d77d23
JasonGrimes99 <48098591+JasonGrimes99@users.noreply.github.com>
2020-10-18 18:40:48 +0100

    feat: add sql

--- DAL/SQL/RequestRepoSql.cs
+++ DAL/SQL/RequestRepoSql.cs
@@ -3,10 +3,29 @@ namespace House.DAL.SQL
 {
     public static class RequestRepoSql
     {
-        public const string SaveRequestItem = @"";
+        public const string SaveRequestItem = @"USE [Dashboard]
+GO
 
-        public const string GetCurrentAmount = @"";
+INSERT INTO [dbo].[Requests]
+           ([Requester]
+           ,[RequestedAmount])
+     VALUES
+           (@Amount
+           ,@Requester)
+GO";
 
-        public const string ExpireRequestItems = @"";
+        public const string GetCurrentAmount = @"USE [Dashboard]
+GO
+
+SELECT SUM(r.RequestedAmount)
+FROM Requests r
+WHERE r.Expired = 0";
+
+        public const string ExpireRequestItems = @"USE [Dashboard]
+GO
+
+UPDATE Requests SET Expired = 1
+
+GO";
     }
 }
